e623aea74c26fed3b84a2268736c2ec2e842416f
전소희 <indian_potato@naver.com>
2023-07-06 21:32:38 +0900

    my first typescript

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { init, exit } from "./myPackage";
+
+init();
+
+// interface Config {
+//   url: string;
+// }
+// declare module "myPackage" {
+//   function init(config: Config): boolean;
+//   function exit(config: number): number;
+// }
